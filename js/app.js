e88a5ab4ae769ca889a159088372fcfbae7eebde
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-05-07 01:23:39 +0800

    up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+new Vivus(
+    "my-svg-m",
+    {
+        duration: 200,
+        file: "./images/template/logo-m.svg",
+    },
+    null
+);
+new Vivus(
+    "my-svg",
+    {
+        duration: 200,
+        file: "./images/template/logo.svg",
+    },
+    null
+);
